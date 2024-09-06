58fa6b84e686b73a5ef48e304abc9b6021a8f70a
Ivan <zhirnovivan1991@gmail.com>
2024-09-06 13:26:14 +0300

    add 2 functions

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,14 @@
-def filter_by_state():
-    pass
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """
+    Функция фильтрует список словарей по значению ключа 'state'.
+    """
+    return [item for item in data if item["state"] == state]
 
-def sort_by_date():
-    pass
+
+def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Функция фильтрует список словарей по значению ключа 'date'
+    по умолчанию список фильтруется по убыванию.
+    """
+
+    return sorted(data, key=lambda x: x["date"], reverse=reverse)
