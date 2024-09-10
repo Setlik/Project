f2d5e90a1cbc16d88f92453b5afde1c1320a09a4
Ivan <zhirnovivan1991@gmail.com>
2024-09-11 00:21:11 +0300

    добавил фикстуры в отдельный файл conftest.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -16,6 +16,6 @@ def test_mask_account_card(value, expected):
     assert mask_account_card(value) == expected
 
 
-def test_get_date():
-    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
-    assert get_date("") == ""
+def test_get_date(empty, dates):
+    assert get_date("2024-03-11T02:26:18.671407") == dates
+    assert get_date("") == empty
